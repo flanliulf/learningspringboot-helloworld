4a723c7899c12f3331a455d2c96df66b240e55fd
liufan <19680460@qq.com>
2019-11-01 16:44:22 +0800

    initial commit

---
+++ src/main/java/com/fancyliu/learningspringboot/LearningspringbootHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.fancyliu.learningspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningspringbootHelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningspringbootHelloworldApplication.class, args);
+    }
+
+}
